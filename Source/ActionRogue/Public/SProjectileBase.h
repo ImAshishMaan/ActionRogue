b4fb09af87375322b3e4f909688e8bf2203f2454
Ashish Maan <ashishmaan22@gmail.com>
2024-03-02 14:08:36 +0530

    Projectile base class

---
+++ Source/ActionRogue/Public/SProjectileBase.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SProjectileBase.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UParticleSystemComponent;
+
+UCLASS(ABSTRACT)
+class ACTIONROGUE_API ASProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* ImpactVFX;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* SphereComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UProjectileMovementComponent* MoveComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UParticleSystemComponent* EffectComp;
+
+	UFUNCTION()
+	virtual void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	// BlueprintNativeEvent = C++ base implementation, can be expanded in Blueprints
+	// BlueprintCallable to allow child classes to trigger explosions
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Explode();
+
+	virtual void PostInitializeComponents() override;
+
+public:	
+
+	ASProjectileBase();
+
+};
